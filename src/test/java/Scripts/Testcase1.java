8351891933692e302c67d38462cff0623d0495be
Mayur <Mayur@LAPTOP-J6DHMLEL>
2022-09-12 17:21:20 +0530

    framework

---
+++ src/test/java/Scripts/Testcase1.java
@@ -0,0 +1,12 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	@Test
+	public void tc1() {
+		Reporter.log("hello java",true);
+	}
+
+}
---
+++ src/test/java/Scripts/Testcase4.java
@@ -0,0 +1,12 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase4 {
+	@Test
+	public void tc1() {
+		Reporter.log("hello python selenium",true);
+	}
+
+}
